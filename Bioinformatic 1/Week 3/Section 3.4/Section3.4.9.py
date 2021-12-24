60999471e6207225d75c24a546348dbe36176ba4
Vu Son luong <sonluong999@gmail.com>
2021-12-24 10:04:17 -0500

    This section is not finished

---
+++ Bioinformatic 1/Week 3/Section 3.4/Section3.4.9.py
@@ -0,0 +1,3 @@
+# Input: A collection of strings Dna and an integer k.
+# Output: A collection Motifs of k-mers, one from each string in Dna, minimizing Score(Motifs) among all possible choices of k-mers.
+
